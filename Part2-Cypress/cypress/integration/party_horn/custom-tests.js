fe6be4581c2c2b78d6e7141b840e2c3f5ebe2779
David Jeong <davidjeong0628@gmail.com>
2021-02-27 21:30:38 -0800

    Part 2 Save

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -29,6 +29,72 @@ describe("Party Horn Tests", () => {
   });
 
   it("Image and sound sources change when party horn radio is selected", () => {
-    cy.get("#radio-party-horn")
+    cy.get("#radio-party-horn").click();
+    cy.get("#sound-image").then(($el) => {
+      expect($el).to.have.attr("src", "./assets/media/images/party-horn.svg");
+    });
+    cy.get("#horn-sound").then(($el) => {
+      expect($el).to.have.attr("src", "./assets/media/audio/party-horn.mp3");
+    });
+  });
+
+  it("Volume image changes when increasing volumes (level 0)", () => {
+    const volumeNumber = cy.get("#volume-number");
+
+    volumeNumber.clear().type("0");
+    cy.get("#volume-image").then(($el) => {
+      expect($el).to.have.attr(
+        "src",
+        "./assets/media/icons/volume-level-0.svg"
+      );
+    });
+  });
+
+  it("Volume image changes when increasing volumes (level 1)", () => {
+    const volumeNumber = cy.get("#volume-number");
+
+    volumeNumber.clear().type("1");
+    cy.get("#volume-image").then(($el) => {
+      expect($el).to.have.attr(
+        "src",
+        "./assets/media/icons/volume-level-1.svg"
+      );
+    });
+  });
+
+  it("Volume image changes when increasing volumes (level 2)", () => {
+    const volumeNumber = cy.get("#volume-number");
+
+    volumeNumber.clear().type("34");
+    cy.get("#volume-image").then(($el) => {
+      expect($el).to.have.attr(
+        "src",
+        "./assets/media/icons/volume-level-2.svg"
+      );
+    });
+  });
+
+  it("Volume image changes when increasing volumes (level 3)", () => {
+    const volumeNumber = cy.get("#volume-number");
+
+    volumeNumber.clear().type("67");
+    cy.get("#volume-image").then(($el) => {
+      expect($el).to.have.attr(
+        "src",
+        "./assets/media/icons/volume-level-3.svg"
+      );
+    });
+  });
+
+  it("Honk button is disabled when the textbox input is an empty or a non-number", () => {
+    cy.get("#volume-number").clear();
+    cy.get("#honk-btn").then(($el) => {
+      expect($el).to.have.prop("disabled", true);
+    });
+
+    cy.get("#volume-number").clear().type("non-number");
+    cy.get("#honk-btn").then(($el) => {
+      expect($el).to.have.prop("disabled", true);
+    });
   });
 });
